573dfe8ab0dba04d0fe42a6fd404fd2b68662bf1
Abdullah Sharef <38993855+Abdullah104@users.noreply.github.com>
2026-01-13 10:57:25 +0300

    read files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{fs::read_dir, io::Read};
+
 fn main() {
-    println!("Hello, world!");
+    let root_test_directory = read_dir("tests").unwrap();
+
+    for root_test_directory_result in root_test_directory {
+        let test_directory = read_dir(root_test_directory_result.unwrap().path()).unwrap();
+
+        for test_result in test_directory {
+            let mut test = std::fs::File::open(test_result.unwrap().path()).unwrap();
+            let mut raw_json = String::new();
+
+            test.read_to_string(&mut raw_json).unwrap();
+
+            println!("{}", raw_json);
+        }
+    }
 }
